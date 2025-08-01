fc08ce499faa52403c79590434e79bff7c54eaf2
shifaliii <shifalilobo9@gmail.com>
2025-08-01 12:13:48 +0530

    frontend completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import notesRoutes from "./routes/notesRoutes.js"
 import {connectDB} from "./config/db.js"
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,8 +15,14 @@ const PORT=process.env.PORT||5001
 
 
 //middleware
+app.use(cors(
+    {
+        origin:"http://localhost:5173",
+    }
+));
 app.use(express.json());
 app.use(rateLimiter);
+
 // app.use((req,res,next)=>{
 //     console.log(`Req method is ${req.method} and Req URL is ${req.url}`);
 //     next();
